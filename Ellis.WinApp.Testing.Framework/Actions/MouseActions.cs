7511f4dc1cf5853be89f0bde7d5aa92f74dd698f
kiran kumar <k3foru@gmail.com>
2014-03-26 05:07:10 -0700

    new actions

--- Ellis.WinApp.Testing.Framework/Actions/MouseActions.cs
+++ Ellis.WinApp.Testing.Framework/Actions/MouseActions.cs
@@ -11,14 +11,12 @@ namespace Ellis.WinApp.Testing.Framework.Actions
     {
         public static void Click(UITestControl control)
         {
-            control.SetFocus();
             Actions.MouseClickOnCoordinates(control);
             Actions.SendEnter();
         }
 
         public static void DoubleClick(UITestControl control)
         {
-            control.SetFocus();
             Mouse.DoubleClick(control);
         }
 
